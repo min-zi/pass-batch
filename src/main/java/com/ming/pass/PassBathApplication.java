abec5e7fa261b1ee83d27c294c97c3d44f1ca3f0
min-zi <min-zi@naver.com>
2025-06-19 20:53:27 +0900

    first commit

---
+++ src/main/java/com/ming/pass/PassBathApplication.java
@@ -0,0 +1,13 @@
+package com.ming.pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassBathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassBathApplication.class, args);
+	}
+
+}
