aa0f5fa9dd1ff358b797b589e2ea6f3baeaf5ed5
권민지 <min-zi@naver.com>
2025-10-15 18:42:41 +0900

    카카오톡 알람 발송을 위해 uuid 필드 추가

--- src/main/java/com/ming/pass/repository/user/UserEntity.java
+++ src/main/java/com/ming/pass/repository/user/UserEntity.java
@@ -4,8 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
+import org.hibernate.annotations.Type;
+
+import com.vladmihalcea.hibernate.type.json.JsonType;
 
 import java.util.Map;
 
@@ -23,13 +24,13 @@ public class UserEntity {
     private UserStatus status;
     private String phone;
 
-    @Column(columnDefinition = "json") //DB 컬럼 타입을 json으로 정의
-    @JdbcTypeCode(SqlTypes.JSON) //DB에 json 형태로 저장된 컬럼 ↔ 자바 객체(Map/List)로 자동 매핑
+    @Type(JsonType.class) // DB 에 json 형태로 저장된 컬럼 ↔ 자바 객체(Map/List)로 자동 매핑
+    @Column(columnDefinition = "json") // Hibernate 가 테이블을 생성할 때 해당 컬럼을 JSON 타입으로 생성
     private Map<String, Object> meta;
 
     public String getUuid() {
         String uuid = null;
-        if (meta.containsKey("uuid")) {  //meta Map 안에 "uuid"라는 key가 있는지 확인
+        if (meta.containsKey("uuid")) {  // meta Map 안에 "uuid" 라는 key 가 있는지 확인
             uuid = String.valueOf(meta.get("uuid"));
         }
         return uuid;
